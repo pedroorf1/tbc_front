8ce71248c92ba2ca251f2b318bf7ebb025792ef8
pedroorf <pedroorf@gmail.com>
2023-12-14 10:24:25 -0300

    config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "tbc - trabalhos freelance e empregos",
-  description: "App para busca de trabalhos freelances e empregos.",
+  title: "tbc - trabalhos freelancer e empregos",
+  description: "App para busca de trabalhos freelancers e empregos.",
 };
 export default function RootLayout({
   children,
